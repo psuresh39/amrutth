198e3d31dc0d7f42e0e6fefc6fdb63c0c2a00c66
unknown <user@user-PC.(none)>
2014-05-25 02:19:28 -0700

    Adding refactored server code: 1. Added 2 classes to act as handlers for 2 different url paths 2. File uses exceptions from exceptions.py

--- foodtruckapi.py
+++ foodtruckapi.py
@@ -1,118 +1,268 @@
+import re
+import ConfigParser
 import json
 from pymongo import MongoClient
 from geopy.geocoders import GoogleV3
 from geopy.distance import vincenty
 from geoip import geolite2
-import re
+from copy import copy
 
-def authhandler():
-    pass
 
-def getfoodtrucks(latitude, longitude, limit, offset, sort, category_filter, radius_filter, max_distance, name, status, fooditems, bounds):
+class FoodTrucks(object):
+    def __init__(self, config_file='amrutth_settings.ini'):
+        self.client = MongoClient()
+        self.db = self.client.test
+        self.foodtrucks = self.db.foodtrucks
+        self.geolocator = GoogleV3()
+        self._config_file = config_file
+        self._config = ConfigParser.RawConfigParser()
+        self.query_parameter = {}
+        self.cache = redis.StrictRedis(host='localhost', port=6379, db=0)
+        if not self._config.read(self._config_file):
+            self._config.add_section('Query Options')
+            self._config.set('Query Options', 'location', json.dumps(None))
+            self._config.set('Query Options', 'bounds', json.dumps(None))
+            self._config.set('Query Options', 'point', json.dumps(None))
+            self._config.set('Query Options', 'limit', json.dumps(40))
+            self._config.set('Query Options', 'maxlimit', json.dumps(100))
+            self._config.set('Query Options', 'offset', json.dumps(0))
+            self._config.set('Query Options', 'sort', json.dumps(0))
+            self._config.set('Query Options', 'category_filter', json.dumps(None))
+            self._config.set('Query Options', 'radius_filter', json.dumps(None))
+            self._config.set('Query Options', 'max_distance', json.dumps(10))
+            self._config.set('Query Options', 'name', json.dumps(None))
+            self._config.set('Query Options', 'status', json.dumps(None))
+            self._config.set('Query Options', 'fooditems', json.dumps(None))
+            with open(self._config_file, 'w') as configfile:
+                configfile.write(self._config_file)
 
-    if bounds is None:
-        if category_filter is None and status is None:
+        if self._config.has_section('Query Option'):
+            self.query_parameter = {option: json.loads(self._config.get("Query Option", option))
+                                    for option in self._config.options('Query Options')}
 
-            if radius_filter is not None:
-                geo_query_result = foodtrucks.find({ "loc" : { "$geoWithin" :{ "$centerSphere" :[ [ longitude, latitude ] , radius_filter / 3959 ]} } } ).limit(limit)
-            else:
-                geo_query_result = foodtrucks.find({ "loc" : { "$near" : [ longitude, latitude ] , $maxDistance: max_distance/69} } ).limit(limit)
-        elif category_filter is not None and status is None:
+    def adjust_limit(self):
+        if self.query_parameter["limit"] > self.query_parameter["maxlimit"]:
+                self.query_parameter["limit"] = self.query_parameter["maxlimit"]
 
-            if radius_filter is not None:
-                geo_query_result = foodtrucks.find({ "loc" : { "$geoWithin" :{ "$centerSphere" :[ [ longitude, latitude ] , radius_filter / 3959 ]} } , "facilitytype":category_filter} ).limit(limit)
-            else:
-                geo_query_result = foodtrucks.find({ "loc" : { "$near" : [ longitude, latitude ] , $maxDistance: max_distance/69}, "facilitytype":category_filter } ).limit(limit)
-        elif category_filter is None and status is not None:
+    def create_multidict(self, *args):
+        if len(args) == 1:
+            return copy(args[0])
+        out = {}
+        for x in args[0]:
+            out[x] = self.create_multidict(*args[1:])
+        return out
 
-            if radius_filter is not None:
-                geo_query_result = foodtrucks.find({ "loc" : { "$geoWithin" :{ "$centerSphere" :[ [ longitude, latitude ] , radius_filter / 3959 ]} } , "status":status} ).limit(limit)
-            else:
-                geo_query_result = foodtrucks.find({ "loc" : { "$near" : [ longitude, latitude ] , $maxDistance: max_distance/69}, "status":status } ).limit(limit)
+
+class NearbyFoodTruckHandler(FoodTrucks):
+
+    def __init__(self):
+        super(NearbyFoodTruckHandler, self).__init__()
+
+    def get_correct_sort_order(self, geo_query_result):
+
+        offset_query_result = list(geo_query_result[self.query_parameter["offset"]:])
+        result = []
+        if not self.query_parameter["name"] and not self.query_parameter["fooditems"]:
+                result = offset_query_result
         else:
-            
-            if radius_filter is not None:
-                geo_query_result = foodtrucks.find({ "loc" : { "$geoWithin" :{ "$centerSphere" :[ [ longitude, latitude ] , radius_filter / 3959 ]} } , "facilitytype":category_filter, "status":status} ).limit(limit)
-            else:
-                geo_query_result = foodtrucks.find({ "loc" : { "$near" : [ longitude, latitude ] , $maxDistance: max_distance/69}, "facilitytype":category_filter, "status":status } ).limit(limit)
-        if name is None and fooditems is None:
-            result = list(geo_query_result)
-        else:
-            if name is not None and fooditems is None:
-                result = [foodtruck for foodtruck in list(geo_query) if re.search(name, foodtruck["applicant"])]
-                if sort == 1:
+            applicant = self.query_parameter["name"] or ".*"
+            fooditems = self.query_parameter["fooditems"] or ".*"
+            result = [foodtruck for foodtruck in offset_query_result
+                      if re.search(fooditems, foodtruck["fooditems"]) and re.search(applicant, foodtruck["applicant"])]
+
+            if self.query_parameter["sort"] == 1:
+                if self.query_parameter["name"]:
                     result = sorted(result, key=lambda x: x["applicant"])
-            elif name is None and fooditems is not None:
-                result = [foodtruck for foodtruck in list(geo_query) if re.search(fooditems, foodtruck["fooditems"])]
-                if sort == 1:
+                else:
                     result = sorted(result, key=lambda x: x["fooditems"])
-            else:
-                result = [foodtruck for foodtruck in list(geo_query) if re.search(fooditems, foodtruck["fooditems"]) and re.search(name, foodtruck["applicant"])]
-                if sort == 1:
-                    result = sorted(result, key=lambda x: x["applicant"])
 
-            return result[offset:]
-    else:
-        for id, coordinate in enumerate(bounds.split("|"))
-            latlang = coordinates.split(",")                    
-            latitude[id] = float(latlang[0])
-            longitude[id] = float(latlang[1])
+        for index, foodtruck in enumerate(result[:]):
+            result[index]["dis"] = vincenty((self.query_parameter["latitude"], self.query_parameter["longitude"]),
+                                            (result[index]["latitude"], result[index]["longitude"])).miles
+        return result
 
-        
+    def generate_basic_bounds_query(self, latitude, longitude):
+        basic_bounds_query = self.create_multidict(['loc'], ['$geowithin'], ['$box'],
+                                                   [[longitude[0], latitude[0]], [longitude[1], latitude[1]]])
+        return basic_bounds_query
 
-
-def searchfoodtruck(location=None, bounds=None, point=None , limit=40, offset=0, sort=0, category_filter=None, radius_filter=None, max_distance=10, name=None, status=None, fooditems=None):
-    if location == None and bounds == None and point == None:
-        location = "current"
-    elif ((location and bounds) or (location and point) or (point and location) or (location and bounds and point)):
-        print("Use only one of the location methods")
-        return
-    else:
-        #Make mongoDB query
-        client = MongoClient()
-        db = client.test
-        foodtrucks = db.foodtrucks
-        geolocator = GoogleV3()
-        if limit > 100:
-            limit = 100
-        if location not None:
-            if location == current:
+    def get_location_coordinates(self):
+        if self.query_parameter["location"]:
+            if self.query_parameter["location"] == "current":
                 match = geolite2.lookup("127.0.0.1")
                 try:
                     latitude, longitude = match.location
                 except Exception as e:
                     print("Exception occured while guessing users ip address ", e)
             else:
-                address, (latitude, longitude) = geolocator.geocode(location)
-            
-            result = getfoodtrucks(latitude, longitude, limit, offset, sort, category_filter, radius_filter, max_distance, name, status, fooditems, bounds)
-
-        elif point not None:
-            coordinates = point.split(",")
+                address, (latitude, longitude) = self.geolocator.geocode(self.query_parameter["location"])
+        elif self.query_parameter["point"]:
+            coordinates = self.query_parameter["point"].split(",")
             latitude = float(coordinates[0])
             longitude = float(coordinates[1])
-            result = getfoodtrucks(latitude, longitude, limit, offset, sort, category_filter, radius_filter, max_distance, name, status, fooditems, bounds)
         else:
-            result = getfoodtrucks(None, None, limit, offset, sort, category_filter, radius_filter, max_distance, name, status, fooditems, bounds)
-	    
+            latitude = {}
+            longitude = {}
+            for idx, coordinate in enumerate(self.query_parameter["bounds"].split("|")):
+                latlang = coordinate.split(",")
+                latitude[idx] = float(latlang[0])
+                longitude[idx] = float(latlang[1])
 
-def getfoodtruckinfo():
-    pass
+        return latitude, longitude
 
-def errorcodes():
-    pass
+    def get_trucks_within_box(self):
+        try:
+            latitude, longitude = self.get_location_coordinates()
+        except Exception:
+            raise InvalidParameterError("Unable to find location")
+        try:
+            query = self.generate_basic_bounds_query(latitude, longitude)
+            if self.query_parameter['category_filter']:
+                query['facilitytype'] = self.query_parameter['category_filter']
 
-def generalapilists():
-    pass
+            if self.query_parameter['status']:
+                query['status'] = self.query_parameter['status']
+        except Exception:
+            raise InternalServerError("Error generating query")
 
-def defaultoptions():
-    pass
+        try:
+            geo_query_result = self.foodtrucks.find(query).limit(self.query_parameter["limit"])
+        except Exception:
+            raise InternalServerError("Error querying database")
+        else:
+            return geo_query_result
 
-def cacheops():
-    pass
+    def generate_radius_query(self, latitude, longitude):
+        return self.create_multidict(["loc"], ["$geoWithin"], ["$centerSphere"],
+                                     [[longitude, latitude], self.query_parameter["radius_filter"] / 3959])
 
-def syncdatabase():
-    pass
+    def generate_distance_query(self, latitude, longitude):
+        loc_query = self.create_multidict(["loc"], ["$near"], [longitude, latitude])
+        distance_query = self.create_multidict(["$maxDistance"], self.query_parameter["max_distance"]/69)
+        for key, value in distance_query.iteritems():
+            loc_query[key] = value
+
+        return loc_query
+
+    def get_trucks_near_point(self):
+        try:
+            latitude, longitude = self.get_location_coordinates()
+        except Exception:
+            raise InvalidParameterError("Unable to find location")
+
+        try:
+            if self.query_parameter["radius_filter"]:
+                query = self.generate_radius_query(latitude, longitude)
+            else:
+                query = self.generate_distance_query(latitude, longitude)
+
+            if self.query_parameter["category_filter"]:
+                query["facilitytype"] = self.query_parameter["category_filter"]
+            if self.query_parameter["status"]:
+                query["status"] = self.query_parameter["status"]
+        except:
+            raise InternalServerError("Error generating query")
+
+        try:
+            geo_query_result = self.foodtrucks.find(query).limit(self.query_parameter["limit"])
+        except:
+            raise InternalServerError("Error querying database")
+        else:
+            return geo_query_result
+
+    def get_all_nearby_foodtrucks(self):
+
+        if self.query_parameter["bounds"]:
+            try:
+                geo_query_result = self.get_trucks_near_point()
+            except Exception:
+                raise
+        else:
+            try:
+                geo_query_result = self.get_trucks_within_box()
+            except Exception:
+                raise
+        try:
+            sorted_result = self.get_correct_sort_order(geo_query_result)
+        except Exception:
+            raise InternalServerError("Error performing sorting operation")
+        return sorted_result
+
+    def search_food_truck(self):
+        result = self.get_cache()
+        if result:
+            return result
+        else:
+            if self.query_parameter["location"] and self.query_parameter["bounds"] and self.query_parameter["point"]:
+                self.query_parameter["location"] = "current"
+            elif ((self.query_parameter["location"] and self.query_parameter["bounds"]) or
+                  (self.query_parameter["location"] and self.query_parameter["point"]) or
+                  (self.query_parameter["point"] and self.query_parameter["location"]) or
+                  (self.query_parameter["location"] and self.query_parameter["bounds"] and self.query_parameter["point"])):
+                raise MissingParameterError("location field is missing in query")
+            else:
+                self.adjust_limit()
+                try:
+                    result = self.get_all_nearby_foodtrucks()
+                except Exception:
+                    raise
+                else:
+                    self.put_cache(json.dumps(result))
+                    return json.dumps(result)
+
+
+class FoodTruckInfoHandler(FoodTrucks):
+
+    def __init__(self):
+        super(FoodTruckInfoHandler, self).__init__()
+
+    def generate_basic_query(self):
+        return self.create_multidict(["$text"], ["$search"], self.query_parameter["name"])
+
+    def generate_sort_query(self):
+        return self.create_multidict(["score"], ["$meta"], "textScore")
+
+    def get_foodtruck_info(self):
+        try:
+            query = self.generate_basic_query()
+            sort_query = self.generate_sort_query()
+            for key, value in sort_query.iteritems():
+                query[key] = value
+        except Exception:
+            raise InternalServerError("Error generating query")
+
+        try:
+            result = self.foodtrucks.find(query).sort(sort_query).limit(self.query_parameter["limit"])
+        except Exception:
+            raise InternalServerError("Error querying database")
+        else:
+            return result
+
+    def get_individual_foodtruck(self):
+        result = self.get_cache()
+        if result:
+            return result
+        else:
+            if not self.query_parameter["name"]:
+                raise MissingParameterError("name field is missing in query")
+            else:
+                self.adjust_limit()
+                try:
+                    result = self.get_foodtruck_info()
+                except Exception:
+                    raise
+                else:
+                    self.put_cache(json.dumps(result))
+                    return json.dumps(result)
+
+    def authhandler(self):
+        pass
+
+    def generalapilists(self):
+        pass
+
+    def syncdatabase(self):
+        pass
 
 if __name__ == "__main__":
     pass
