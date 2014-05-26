1f4e20e97a64d8daa5838aa697f1732fa572eed7
root <root@amrutth.goz.cm>
2014-05-25 22:22:45 -0700

    Adding new exceptions file

---
+++ foodtruckexceptions.py
@@ -0,0 +1,32 @@
+class FoodTruckError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class MissingParameterError(FoodTruckError):
+    code = 1001
+    http_code = 400
+    def __init__(self, msg="parameter is missing in query"):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+
+
+class InvalidParameterError(FoodTruckError):
+    code = 1001
+    http_code = 400
+    def __init__(self, msg="parameter is invalid"):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+
+
+class InternalServerError(FoodTruckError):
+    code = 1002
+    http_code = 500
+    def __init__(self, msg="internal server error"):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
+
+
